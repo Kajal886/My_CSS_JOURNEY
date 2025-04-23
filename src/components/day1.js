6a052dc465fd2ab2ec9c9b4663fdecae12ae8f26
Kajal886 <kajalkajal1406@gmail.com>
2025-04-23 19:28:01 +0530

    Day1:::  Setup project and created Day1 component with basic styles

---
+++ src/components/day1.js
@@ -0,0 +1,14 @@
+
+import './Day1.css';
+
+function Day1() {
+  return (
+    <div className="day1-container">
+      <h1>Welcome to CSS Day 1</h1>
+      <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development. Its purpose is to permit a page layout to be designed, independently of the copy that will subsequently populate it, or to demonstrate various fonts of a typeface without meaningful text that could be distracting.</p>
+      <button onClick={() => alert('Nice! You finished Day 1!')}>Click Me</button>
+    </div>
+  );
+}
+
+export default Day1;
