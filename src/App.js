05e8db9f59a18dd40db0383683aeac5db9ef9d04
Kajal886 <kajalkajal1406@gmail.com>
2025-04-24 20:35:58 +0530

    Create 2-column Flexbox layout in Day2 component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import CssBasic from './components/day1';
+import Day2 from './components/Day2';
 function App() {
   return (
     <div className="App">
       <CssBasic/>
+      <Day2/>
     </div>
   );
 }
---
+++ src/components/Day2.js
@@ -0,0 +1,25 @@
+import './Day2.css';
+
+function Day2() {
+  return (
+    <>
+     <div>
+            <h1>Welcome to CSS Day 2</h1>
+        </div>
+  
+    <div className="flex-container">
+       
+      <div className="left-panel">
+        <h2>Sidebar</h2>
+        <p>This is the sidebar content.</p>
+      </div>
+      <div className="right-panel">
+        <h2>Main Content</h2>
+        <p>This is the main content area.</p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Day2;
